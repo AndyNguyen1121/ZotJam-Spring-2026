b0d80d8a3e8b5577dd58d57ed16ca77093e063a7
chiken <isabellehopeeriksson@gmail.com>
2026-04-10 18:00:34 -0700

    Camera border colors in PlayerSwitch

--- Assets/Scripts/PlayerSwitchManager.cs
+++ Assets/Scripts/PlayerSwitchManager.cs
@@ -1,6 +1,8 @@
 using Player;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.UI;
+using UnityEngine.UI;
 
 public class PlayerSwitchManager : MonoBehaviour
 {
@@ -10,8 +12,9 @@ public class PlayerSwitchManager : MonoBehaviour
     
     [SerializeField] private PlayerMovementManager goobert_MM;
     [SerializeField] private PlayerMovementManager gort_MM;
-    
-    [SerializeField] 
+
+    [SerializeField] private Image goobert_UI;
+    [SerializeField] private Image gort_UI;
     
     void Start()
     {
@@ -19,6 +22,10 @@ public class PlayerSwitchManager : MonoBehaviour
         goobert_MM.enabled = true;
         gort_PI.enabled = false;
         gort_MM.enabled = false;
+
+        goobert_UI.color = Color.white;
+        gort_UI.color = new Color(0.4941177f, 0.7843138f, 0.7764707f, 1f);
+
     }
 
     void Update()
@@ -30,6 +37,10 @@ public class PlayerSwitchManager : MonoBehaviour
             gort_PI.enabled = !gort_PI.enabled;
             gort_MM.enabled = !gort_MM.enabled;
             
+            Color temp = goobert_UI.color;
+            goobert_UI.color = gort_UI.color;
+            gort_UI.color = temp;
+
         }
     }
 }
